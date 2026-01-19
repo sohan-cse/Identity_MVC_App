959618563424e8f0777db4a6ca34d145dc93882c
Md. Ibn Masud <sohan.cse.pust@gmail.com>
2026-01-20 01:28:08 +0600

    Add Registration form

---
+++ Identity_MVC_App/Controllers/LoginController.cs
@@ -0,0 +1,55 @@
+﻿using Identity_MVC_App.Models;
+using Identity_MVC_App.ViewModels;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Identity_MVC_App.Controllers
+{
+    public class LoginController : Controller
+    {
+        private readonly UserManager<Users>_userManager;
+        private readonly SignInManager<Users>_signInManager;
+
+        public LoginController(UserManager<Users> userManager, SignInManager<Users> signInManager)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+        }
+
+        [HttpGet]
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult>Register(RegisterViewModel model)
+        {
+            if(ModelState.IsValid)
+            {
+                Users user = new Users()
+                {
+                    Name = model.Name,
+                    Email = model.Email,
+                    NormalizedEmail = model.Email,
+                    UserName=model.Email,
+                    NormalizedUserName=model.Email
+                };
+                var result= await _userManager.CreateAsync(user,model.Password);
+
+                if(result.Succeeded)
+                {
+                    return RedirectToAction("Index","Home");
+                }
+                else
+                {
+                    foreach(var error in result.Errors)
+                    {
+                        ModelState.AddModelError("",error.Description);
+                    }
+                }
+            }
+            return View(model);
+        }
+    }
+}
